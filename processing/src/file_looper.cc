43ae80a91d153fa7d30027b531a9e3960d3cd43e
GilesStrong <giles.strong@outlook.com>
2020-05-15 17:55:22 +0100

    rename

--- processing/src/file_looper.cc
+++ processing/src/file_looper.cc
@@ -585,9 +585,9 @@ std::map<unsigned long, std::string> FileLooper::build_id_map(TFile* in_file) {
 double FileLooper::_get_weight(TTreeReaderValue<std::vector<double>>& rv_weight, const std::vector<unsigned int>& idxs) {
     double del, weight = (*rv_weight)[idxs[0]];
     for (unsigned int i = 1; i < idxs.size(); i++) {
-        del = std::abs((*rv_weight)[i]-weight);
-        if (del < 1e-7) {
-            std::cout << "Multiple weights found. Del = " << del << "\n";
+        del = std::abs((*rv_weight)[i]-weight)/weight;
+        if (del < 1e-3) {
+            std::cout << "Multiple weights found. " << rv_weight[i] << " : " << weight << " Del = " << del << "\n";
             assert(false);
         }
     }
@@ -597,9 +597,9 @@ double FileLooper::_get_weight(TTreeReaderValue<std::vector<double>>& rv_weight,
 float FileLooper::_get_mva_score(TTreeReaderValue<std::vector<float>>& rv_mva_score, const std::vector<unsigned int>& idxs) {
     float del, mva_score = (*rv_mva_score)[idxs[0]];
     for (unsigned int i = 1; i < idxs.size(); i++) {
-        del = std::abs((*rv_mva_score)[i]-mva_score);
-        if (del < 1e-7) {
-            std::cout << "Multiple mva scores found. Del = " << del << "\n";
+        del = std::abs((*rv_mva_score)[i]-mva_score)/mva_score;
+        if (del < 1e-3) {
+            std::cout << "Multiple mva scores found. " << rv_mva_score[i] << " : " << mva_score << " << del << " << del << "\n";
             assert(false);
         }
     }
