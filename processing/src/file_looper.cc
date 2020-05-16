6d064d7a11ac9fdfe48aed92ac94fddd1fa642fb
GilesStrong <giles.strong@outlook.com>
2020-05-16 12:59:53 +0100

    fixes

--- processing/src/file_looper.cc
+++ processing/src/file_looper.cc
@@ -552,27 +552,27 @@ bool FileLooper::_accept_evt(const int& region, const bool& central_unc, const i
         return false; // Only consider klambda at SM point
     }
     if (_apply_cut && !cut_pass) {
-        std::cout << "Rejecting due to mass cut = " << cut_pass << "\n";
+        // std::cout << "Rejecting due to mass cut = " << cut_pass << "\n";
         return false;  // Require mass cut and cut failed
     }
     if (!_inc_data && class_id == -1) {
-        std::cout << "Rejecting due to class ID = " << class_id << "\n";
+        // std::cout << "Rejecting due to class ID = " << class_id << "\n";
         return false; // Don't include data and event is data
     }
     if (!_inc_other_regions && region != 0) {
-        std::cout << "Rejecting due to region = " << region << "\n";
+        // std::cout << "Rejecting due to region = " << region << "\n";
         return false;  //Don't include other regions and event is not SS Iso
     }
     if (!_inc_unc && !central_unc) {
-        std::cout << "Rejecting due to central = " << central_unc << "\n";
+        // std::cout << "Rejecting due to central = " << central_unc << "\n";
         return false;  //Don't systematics and event is a systematic
     }
     if (!_inc_all_jets && jet_cat == 0) {
-        std::cout << "Rejecting due to jet_cat = " << jet_cat << "\n";
+        // std::cout << "Rejecting due to jet_cat = " << jet_cat << "\n";
         return false;  // Only use inference category jets and event is non-inference category
     }
     if (_only_sm_vbf && (cv != 1 || c2v != 1 || c3 != 1)) {
-        std::cout << "Rejecting due to cv = " << cv << " c2v = " << c2v << " c3 = " << c3 << "\n";
+        // std::cout << "Rejecting due to cv = " << cv << " c2v = " << c2v << " c3 = " << c3 << "\n";
         return false; // Only consider SM VBF
     }
     return true;
